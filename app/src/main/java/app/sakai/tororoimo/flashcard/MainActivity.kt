4e5271acf43070627b150889a74f165d1e302495
Aogon <aoi_sakai@icloud.com>
2020-05-16 11:43:28 +0900

    check2 配列とfor文の利用

--- app/src/main/java/app/sakai/tororoimo/flashcard/MainActivity.kt
+++ app/src/main/java/app/sakai/tororoimo/flashcard/MainActivity.kt
@@ -2,7 +2,6 @@ package app.sakai.tororoimo.flashcard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -14,26 +13,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val apple = CustomWord(R.drawable.apple, "りんご", "apple")
-        val banana = CustomWord(R.drawable.banana, "バナナ", "banana")
-        val orange = CustomWord(R.drawable.orange, "オレンジ", "orange")
-        val strawberry = CustomWord(R.drawable.strawberry, "いちご", "strawberry")
+        val customWords = arrayOfNulls<CustomWord>(4)
 
+        customWords[0] = CustomWord(R.drawable.apple, "りんご", "apple")
+        customWords[1] = CustomWord(R.drawable.banana, "バナナ", "banana")
+        customWords[2] = CustomWord(R.drawable.orange, "オレンジ", "orange")
+        customWords[3] = CustomWord(R.drawable.strawberry, "いちご", "strawberry")
 
-        addWord(apple)
-        addWord(banana)
-        addWord(orange)
-        addWord(strawberry)
+        for (i in 0..3) {
+            addWord(customWords[i])
+        }
 
 
     }
 
-    private fun addWord(CustomWord: CustomWord) {
+    private fun addWord(CustomWord: CustomWord?) {
         val nameTextView1 = TextView(applicationContext)
-        nameTextView1.text = CustomWord.japanese
+        nameTextView1.text = CustomWord?.japanese
 
         val nameTextView2 = TextView(applicationContext)
-        nameTextView2.text = CustomWord.english
+        nameTextView2.text = CustomWord?.english
 
 
 //        container.addView(nameTextView)
@@ -41,7 +40,7 @@ class MainActivity : AppCompatActivity() {
         val layout = LinearLayout(this.applicationContext)
         layout.orientation = LinearLayout.HORIZONTAL
         val imageView = ImageView(this)
-        imageView.setImageResource(CustomWord.resId)
+        imageView.setImageResource(CustomWord?.resId!!)
         imageView.adjustViewBounds = true
         imageView.maxHeight = 200
 
